0ed4152070a5f88b104b1b348acce96a5bc94fba
David Marques <davidmarques.261299@gmail.com>
2025-09-09 00:29:31 -0300

    feat: Folter static

--- src/server.routes.ts
+++ src/server.routes.ts
@@ -1,5 +1,19 @@
+import path from 'path';
+import { existsSync, mkdirSync } from 'fs';
+import { fastifyStatic } from '@fastify/static';
+
 import { fastify } from './server.config';
 import categoryRoutes from './routes/categories';
+
+const dirPublic = path.join(__dirname, '..', 'public');
+if (!existsSync(dirPublic)) {
+  mkdirSync(dirPublic);
+}
+
+fastify.register(fastifyStatic, {
+  root: dirPublic,
+  prefix: '/',
+});
 
 fastify.register(categoryRoutes, { prefix: '/categories' });
 
